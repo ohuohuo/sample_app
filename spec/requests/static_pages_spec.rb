cd62011af0a82ff4e34b157e61ca386649a8fed5
ohuohuo <zczou@hotmail.com>
2014-09-09 23:40:28 -0400

    Exercise

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,7 +45,14 @@ end
 end
 end
 
-
-
-
+  describe "Contact" do
+     it "should have the content 'content'" do
+       visit '/static_pages/contact'
+       expect(page).to have_content("Contact")
+end
+     it "should have the title 'Contact'" do
+       visit '/static_pages/contact'
+       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+end
+end
 end
