75871732fb6f8907ecbe6f8812d0c6524f456c6f
ohuohuo <zczou@hotmail.com>
2014-09-29 23:15:09 -0400

    Finish the StaticPage stage

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,55 +12,36 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
-let(:base_title){"Ruby on Rails Tutorial Sample App"}
+#let(:base_title){"Ruby on Rails Tutorial Sample App"}
+
+subject {page}
 
 describe "Home Page" do 
-    it "should have the content 'Sample App'" do
-       visit root_path
-       expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    before { visit root_path } #before还可以使用别名before(:each)调用 
+    
+    it {should have_content('Sample App')}
+    it {should have_title(full_title(''))}
+    it {should_not have_title('| Home')}
 end
 
-   it "should not have a custom page title" do
-     visit root_path
-     expect(page).not_to have_content('| Home')
-end
-end
-
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-end
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("#{base_title} | Help")
-end
+describe "Help page" do
+    before {visit help_path}
+   
+    it {should have_content('Help')}
+    it {should have_title(full_title('Help'))}
 end
 
-  describe "About page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-end
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About Us")
-end
+describe "About page" do
+    before {visit about_path}
+    
+    it {should have_content('About')}
+    it {should have_title(full_title('About Us'))}
 end
 
-  describe "Contact" do
-     it "should have the content 'content'" do
-       visit contact_path
-       expect(page).to have_content("Contact")
-end
-     it "should have the title 'Contact'" do
-       visit contact_path
-       expect(page).to have_title("#{base_title} | Contact")
-end
+describe "Contact" do
+   before {visit contact_path}
+   
+   it {should have_content('Contact')}
+   it {should have_title(full_title('Contact'))}
 end
 end
